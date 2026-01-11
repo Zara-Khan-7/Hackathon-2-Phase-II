a6f07f99626e9cf3177cbae108ee5e0e39fb62aa
Zara Yousuf Khan <zaara.khan1795.zy@gmail.com>
2026-01-11 05:51:52 +0500

    Fix auth route - force dynamic rendering

--- frontend/src/app/api/auth/[...all]/route.ts
+++ frontend/src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,7 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
+// Force dynamic rendering - don't try to build at compile time
+export const dynamic = "force-dynamic";
+
 export const { GET, POST } = toNextJsHandler(auth);
